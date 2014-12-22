bdb3387c00ff2ea6a5f93d74d16028359a8d27f6
bosiam <wangzb1990@gmail.com>
2014-12-22 23:09:29 +0800

    ci Daily

--- views/layouts/textwizard.php
+++ views/layouts/textwizard.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'textwizard.cn|文本巫',
+                'brandLabel' => 'textwizard.cn|编码助手',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
